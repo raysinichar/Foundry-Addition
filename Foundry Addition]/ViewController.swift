d555657bea49b413e9852dcd6f6216c1d0789b99
raysinichar <raysinichar@gmail.com>
2014-12-07 18:45:07 -0500

    messgage

--- Foundry Addition]/ViewController.swift
+++ Foundry Addition]/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //HELLO!
         //Hi
+        //this is my first change
     }
 
     override func didReceiveMemoryWarning() {
