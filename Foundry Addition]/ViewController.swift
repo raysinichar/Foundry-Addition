d153759d7ec331d996cd8ef3bfa08bafccc5fa37
raysinichar <raysinichar@gmail.com>
2014-12-07 18:28:24 -0500

    Initial Commit

---
+++ Foundry Addition]/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foundry Addition]
+//
+//  Created by Ray Andrew Sinichar on 12/7/14.
+//  Copyright (c) 2014 Ray Andrew Sinichar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
