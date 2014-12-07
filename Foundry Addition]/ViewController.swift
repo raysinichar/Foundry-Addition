69a93f032bce58604d0546e9c31075653dfb3fb0
wzvish <VishnuOSX@Vishnus-MBP.home>
2014-12-07 18:44:04 -0500

    lets do eet

--- Foundry Addition]/ViewController.swift
+++ Foundry Addition]/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //HELLO!
         //Hi
+        //git me some friends
     }
 
     override func didReceiveMemoryWarning() {
